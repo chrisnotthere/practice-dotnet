eca4411a68e81b86e312923da6b2591792849368
chris <mr.chrisnash@gmail.com>
2024-02-28 10:35:57 -0800

    FinShark setup

--- DotNetAPI/obj/Debug/net8.0/DotNetAPI.AssemblyInfo.cs
+++ DotNetAPI/obj/Debug/net8.0/DotNetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ff1170dffbb8d79acc946c98964ac89af687b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8df23fb79511c4ace09264e254c13ddfc615c697")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
